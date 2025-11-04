54555417011b7b85c113531447796f254728c833
Abdolrezakalantari <Abdolrezakalantari2024@gmail.com>
2025-11-04 12:23:01 +0400

    Add project files.

---
+++ QRCodeService.API/Controllers/QrController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QRCodeService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QrController : ControllerBase
+    {
+        private readonly QRCodeService.Service.QrCodeGeneratorService _qrCodeGeneratorService;
+        public QrController(QRCodeService.Service.QrCodeGeneratorService qrCodeGeneratorService)
+        {
+            _qrCodeGeneratorService = qrCodeGeneratorService;
+        }
+
+        [HttpGet]
+        public IActionResult GetQr([FromQuery] string url)
+        {
+            if (string.IsNullOrWhiteSpace(url)) return BadRequest("URL is required");
+
+            var qrBytes = _qrCodeGeneratorService.GenerateQrCode(url);
+            return File(qrBytes, "image/png", "qrcode.png");
+        }
+    }
+}
---
+++ QRCodeService.Service/QrCodeGeneratorService.cs
@@ -0,0 +1,21 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace QRCodeService.Service
+{
+    public class QrCodeGeneratorService
+    {
+        public byte[] GenerateQrCode(string url)
+        {
+            using var qrGenerator = new QRCoder.QRCodeGenerator();
+            var qrCodeData = qrGenerator.CreateQrCode(url, QRCoder.QRCodeGenerator.ECCLevel.Q);
+            using var qrCode = new PngByteQRCode(qrCodeData);
+            return qrCode.GetGraphic(20);
+        }
+    }
+}
